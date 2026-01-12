da8c51061322d306d5e295fddc4d4d60d235fed8
Timothy9O <trajewitz@gmail.com>
2026-01-12 09:50:24 +0000

    Variablen u. Konstanten

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -15,12 +15,12 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName) // Ausgabe
-firstName = "Max"; // Wertzuweisung / assignment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausgabe
+//let firstName; // Deklaration (Definition)
+//console.log(firstName) // Ausgabe
+//firstName = "Max"; // Wertzuweisung / assignment
+//console.log(firstName); // Ausgabe
+//let familyName = "Mütze"; // Deklaration + Wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausgabe
 
 
 /*  
@@ -30,8 +30,33 @@ console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausgabe
 
 
 
+//let tets; // Deklaration
 
-
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log
+//console.log
 
 
 /* Variablen vs. Konstanten in JS */;
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisiereung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+
+
+
+
+
+
+
+
+
+// Konstante
+const test2 = "hi!";
+//test2 = "hello"; // Fehler! Überschreiben nicht möglich
+console.log("Inhalt: " + test2); // Ausgabe
+
+
